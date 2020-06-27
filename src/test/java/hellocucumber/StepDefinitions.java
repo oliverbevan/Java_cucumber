4970589ce330fa6a6df5981a0d97e1cb2be3c68d
oliver bevan <oliverbevan@outlook.com>
2020-06-27 16:34:37 +0100

    Updated test to be based on actual day

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -4,6 +4,8 @@ package hellocucumber;
         import io.cucumber.java.en.Then;
         import io.cucumber.java.en.When;
 
+        import java.util.Date;
+
         import static org.junit.Assert.*;
 
 class IsItFriday {
@@ -16,9 +18,44 @@ public class StepDefinitions {
     private String today;
     private String actualAnswer;
 
-    @Given("today is Sunday")
-    public void today_is_Sunday() {
-        today = "Sunday";
+    @Given("today is Not Friday")
+    public void today_is_Sunday() throws Exception {
+        Date date = new Date();
+        int day;
+        day = date.getDay();
+
+        switch (day) {
+            case 0:
+                today = "Sunday";
+                System.out.println(today);
+                break;
+            case 1:
+                today = "Monday";
+                System.out.println(today);
+                break;
+            case 2:
+                today = "Tuesday";
+                System.out.println(today);
+                break;
+            case 3:
+                today = "Wednesday";
+                System.out.println(today);
+                break;
+            case 4:
+                today = "Thursday";
+                System.out.println(today);
+                break;
+            case 5:
+                today = "Friday";
+                throw new Exception("IT IS "+today+"!!!!!!!!!!!!!!!!!");
+            case 6:
+                today = "Saturday";
+                System.out.println("Today it is "+today);
+                break;
+        }
+
+        System.out.println(today);
+
     }
 
     @When("I ask whether it's Friday yet")
@@ -28,6 +65,8 @@ public class StepDefinitions {
 
     @Then("I should be told {string}")
     public void i_should_be_told(String expectedAnswer) {
+
+
         assertEquals(expectedAnswer, actualAnswer);
     }
 
--- target/test-classes/hellocucumber/is_it_friday_yet.feature
+++ target/test-classes/hellocucumber/is_it_friday_yet.feature
@@ -2,6 +2,6 @@ Feature: Is it Friday yet?
   Everybody wants to know when it's Friday
 
   Scenario: Sunday isn't Friday
-    Given today is Sunday
+    Given today is Not Friday
     When I ask whether it's Friday yet
     Then I should be told "Nope"
